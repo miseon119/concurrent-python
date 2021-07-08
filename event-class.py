ae6e1647383d2a2d7af65338a378001acfe35ea6
Wendy Park <piaomeixian119@hotmail.com>
2021-07-08 13:40:41 +0900

    Create event-class.py

---
+++ event-class.py
@@ -0,0 +1,32 @@
+class Event(object):
+  
+    def __init__(self):
+        self.__eventhandlers = []
+  
+    def __iadd__(self, handler):
+        self.__eventhandlers.append(handler)
+        return self
+  
+    def __isub__(self, handler):
+        self.__eventhandlers.remove(handler)
+        return self
+  
+    # def __call__(self, *args, **keywargs):
+    #     for eventhandler in self.__eventhandlers:
+    #         eventhandler(*args, **keywargs)
+    def fire(self, *args, **keywargs):
+        for eventhandler in self.__eventhandlers:
+            eventhandler(*args, **keywargs)            
+
+class AbcEvent(object):
+    def __init__(self):
+        self.OnChange=Event()
+          
+    def fire(self, udata):
+        self.OnChange.fire(udata)
+          
+    def AddSubscribersForEvent(self,objMethod):
+        self.OnChange += objMethod
+          
+    def RemoveSubscribersForEvent(self,objMethod):
+        self.OnChange -= objMethod            
